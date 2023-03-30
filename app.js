e2defa4e0bf882df46258cd794063a32f0624a5d
FIRST_NAME LAST_NAME <subhajit.bhattacharyya@gmail.com>
2023-03-30 19:35:24 +0530

    2nd commit

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const http=require('http')
+const { type } = require('os')
 const server =http.createServer((req,res)=>{
     console.log('subhajit')
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.write('My name is subhajit')
+    res.end()
 })
 server.listen(4000)
